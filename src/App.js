ab115cd9c415403bf55f6fc739fef93488ca2df4
Muhammad Alfani <alfaniaulia09@gmail.com>
2023-12-13 14:03:10 +0700

    Adding Hero Sectiron and Menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
 import Nav from './components/Nav';
+import Header from './components/Header';
+import Menu from './components/Menu';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
       <Nav />
+      <Header />
+      <Menu />
     </>
   );
 }
